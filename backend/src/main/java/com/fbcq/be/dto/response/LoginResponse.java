84c5724f6a73cceae230859d0d0eae487eb0d289
kangmin <lnewgatel@gmail.com>
2025-01-12 01:59:38 +0900

    Feat: 이메일 확인 로직 추가 및 비밀번호 복호화 처리 및 검증 반영

--- backend/src/main/java/com/fbcq/be/dto/response/LoginResponse.java
+++ backend/src/main/java/com/fbcq/be/dto/response/LoginResponse.java
@@ -2,6 +2,7 @@ package com.fbcq.be.dto.response;
 
 public record LoginResponse(
         Long userId,
-        String name
+        String name,
+        String password // 내부 검증용
 ) {
-};
+}
--- backend/src/main/java/com/fbcq/be/service/UserServiceImpl.java
+++ backend/src/main/java/com/fbcq/be/service/UserServiceImpl.java
@@ -1,31 +1,61 @@
 package com.fbcq.be.service;
 
+import com.fbcq.be.domain.User;
 import com.fbcq.be.mapper.UserMapper;
 import com.fbcq.be.dto.request.LoginRequest;
 import com.fbcq.be.dto.request.SignUpRequest;
 import com.fbcq.be.dto.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserMapper userDao;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
-        return userDao.select(loginRequest);
+        // 이메일로 사용자 정보 조회
+        User user = userDao.selectUserByEmail(loginRequest.email());
+        if (user == null || !passwordEncoder.matches(loginRequest.password(), user.getPassword())) {
+            log.warn("Invalid login attempt for email: {}", loginRequest.email());
+            return null; // 인증 실패
+        }
+
+        // 성공 시 LoginResponse 생성
+        return userDao.selectLoginResponseByEmail(loginRequest.email());
     }
+
+
 
     @Override
     public int signUp(SignUpRequest signUpRequest) {
-        return userDao.create(signUpRequest);
+        // 비밀번호 해싱
+        String hashedPassword = passwordEncoder.encode(signUpRequest.password());
+
+        // User 객체 생성 (DTO -> 엔터티 변환)
+        User user = User.fromSignUpRequest(signUpRequest, hashedPassword);
+
+        // 데이터베이스 저장
+        return userDao.insertUser(user);
     }
+
 
     @Override
     public boolean updatePasswordByEmail(String email, String password) {
-//        String hashedPassword = passwordEncoder.encode(plainPassword);
-        return userDao.updatePasswordByEmail(email, password);
+        String hashedPassword = passwordEncoder.encode(password);
+        return userDao.updatePasswordByEmail(email, hashedPassword);
     }
-
+    @Override
+    public boolean checkEmailExists(String email) {
+        User existUser = userDao.selectUserByEmail(email);
+        if (existUser != null) {
+            return true;
+        }
+        return false;
+    }
 }
